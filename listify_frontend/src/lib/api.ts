09fd787b9f73ff7e078f234481e78f3b87868e0f
Uraneptus <50745606+Uraneptus@users.noreply.github.com>
2026-07-28 23:31:33 +0200

    include method

--- listify_frontend/src/lib/api.ts
+++ listify_frontend/src/lib/api.ts
@@ -1,19 +1,20 @@
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8081/api';
 
 export async function apiGetJson<T>(path: string): Promise<T> {
-  const response = await apiGet(path);
+  const response = await callApi(path, "GET");
 
   return response.json() as Promise<T>;
 }
 
 export async function apiGetText(path: string): Promise<string> {
-  const response = await apiGet(path);
+  const response = await callApi(path, "GET");
 
   return response.text() as Promise<string>;
 }
 
-async function apiGet(path: string) {
+export async function callApi(path: string, method: string) {
   const response = await fetch(`${apiBaseUrl}${path}`, {
+    method: method,
     credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
